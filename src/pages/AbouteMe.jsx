f3c1c6f6cdc3c71cc2e0a38c2f02d218e29f00d9
Alex09121995 <158055482+Alex09121995@users.noreply.github.com>
2025-07-07 21:26:34 -0400

    aboute me se modifico

--- src/pages/AbouteMe.jsx
+++ src/pages/AbouteMe.jsx
@@ -9,23 +9,23 @@ import { Navegation } from "../Componentes/BarNavegation"
 
 export default function Home() {
   return (
-    <article className="w-[99%] md:w-[70%] h-fit border-6 border-l-0 border-color  rounded-r-2xl flex  flex-col md:flex-row gap-4 moveRight bg-[#121212]/50  ">
+    <article className="w-[99%] md:w-[70%] h-fit md:h-[80%] border-6 border-l-0 border-color  rounded-r-2xl flex  flex-col md:flex-row gap-4 moveRight bg-[#121212]/50  ">
         <div className="md:h-[100%]  w-[100%] md:w-fit flex justify-center md:items-center">
             <img src={FotoAnimada} alt="" className=" shadow-xl shadow-[#000000] p-2 border-color rounded-3xl md:w-fit w-[200px] md:rounded-r-2xl md:h-fit h-[200px] " />
         </div>
       <div className="text-white flex justify-center mt-4 mb-4 md:mt-[10%] relative ">
         <IconChevronLeft stroke={3} size={40} className="absolute md:left-5 left-[60px] aparecer title-color" />
         <h1>
-          <span className="text-color md:text-2xl efecto1 md:left-[60px] left-[95px] text-[28px] font-extrabold absolute "> 
+          <span className="text-color md:text-3xl efecto1 md:left-[60px] left-[95px] text-[28px] font-extrabold absolute "> 
             Adrian
           </span>
-          <span className="text-color md:text-2xl efecto2 md:left-[185px] hidden md:flex  left-[140px] text-[28px] font-extrabold   absolute"> 
+          <span className="text-color md:text-3xl efecto2 md:left-[185px] hidden md:flex  left-[140px] text-[28px] font-extrabold   absolute"> 
             Alexis
           </span>
-          <span className="text-color md:text-2xl efecto3 md:left-[310px] left-[200px]  text-[28px] font-extrabold  absolute">
+          <span className="text-color md:text-3xl efecto3 md:left-[310px] left-[200px]  text-[28px] font-extrabold  absolute">
             Lopez
           </span>
-          <span className="text-color md:text-2xl efecto4 md:left-[420px] hidden md:flex right-[60px] font-extrabold text-[28px]  absolute">
+          <span className="text-color md:text-3xl efecto4 md:left-[420px] hidden md:flex right-[60px] font-extrabold text-[28px]  absolute">
             Santos
           </span>
         </h1>
