fcbf40a42b55bff9cbad8bc1429605d7c5660a51
Alex09121995 <158055482+Alex09121995@users.noreply.github.com>
2025-07-07 21:02:52 -0400

    se modifico contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -10,13 +10,13 @@ export const Contact = () =>{
                     <li>
                         <a className='flex items-center  ease-in-out active:scale-90  efecto1' href='https://www.linkedin.com/in/adri%C3%A1n-a-lopez-santos-669ba4333?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target="_blank">
                             <IconBrandLinkedin stroke={2} size={60} className='animate-[linkColor_4s_ease-in-out_infinite]' />
-                            <h2 className='text-3xl hover:text-[#162556]'>Linkedin</h2>
+                            <h2 className='text-2xl hover:text-[#162556]'>Linkedin</h2>
                         </a>
                     </li>
                     <li>
                         <a className='flex items-center  active:scale-90  efecto2'  href="https://github.com/Alex09121995/Curso-fullstack-talenting" target="_blank">
                             <IconBrandGithub stroke={2} size={60} className='animate-[gitColor_4s_ease-in-out_infinite]'  /> 
-                            <h2 className='text-3xl hover:text-[#000000]'>Github</h2>
+                            <h2 className='text-2xl hover:text-[#000000]'>Github</h2>
 
                         </a>
                     </li>
@@ -25,13 +25,13 @@ export const Contact = () =>{
                     <li>
                         <a className='flex items-center efecto4' href='https://wa.me/18092322812' target="_blank">
                             <IconBrandWhatsapp stroke={2} size={60} className='animate-[wsColor_4s_ease-in-out_infinite]'  />
-                            <h2 className='text-3xl hover:text-[#08aa5e] '> Whatsapp</h2>
+                            <h2 className='text-2xl hover:text-[#08aa5e] '> Whatsapp</h2>
                         </a>
                     </li>
                     <li> 
                         <a className='flex items-center active:scale-90  efecto3' href="mailto:Alexaghm411@gmail.com?subject=Hola&body=Quiero contactarte" target="_blank">
                             <IconMail stroke={2} size={60} className='animate-[emailColor_4s_ease-in-out_infinite]' />
-                            <h2 className='text-3xl hover:text-[#f5493b]'>Gmail</h2>
+                            <h2 className='text-2xl hover:text-[#f5493b]'>Gmail</h2>
 
                         </a>
                     </li>
