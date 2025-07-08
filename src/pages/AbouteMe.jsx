5036e3119fc2005b87a6af9fd0c428b2b2175829
Alex09121995 <158055482+Alex09121995@users.noreply.github.com>
2025-07-07 21:11:15 -0400

    se modifico home page

--- src/pages/AbouteMe.jsx
+++ src/pages/AbouteMe.jsx
@@ -14,23 +14,23 @@ export default function Home() {
             <img src={FotoAnimada} alt="" className=" shadow-xl shadow-[#000000] p-2 border-color rounded-3xl md:w-fit w-[200px] md:rounded-r-2xl md:h-fit h-[200px] " />
         </div>
       <div className="text-white flex justify-center mt-4 mb-4 md:mt-[10%] relative ">
-        <IconChevronLeft stroke={3} size={40} className="absolute md:left-5 left-0 aparecer title-color" />
+        <IconChevronLeft stroke={3} size={40} className="absolute md:left-5 left-[65px] aparecer title-color" />
         <h1>
-          <span className="text-color md:text-2xl efecto1 md:left-[60px] left-[40px] text-[28px] font-extrabold absolute "> 
+          <span className="text-color md:text-2xl efecto1 md:left-[60px] left-[100px] text-[28px] font-extrabold absolute "> 
             Adrian
           </span>
-          <span className="text-color md:text-2xl efecto2 md:left-[185px] left-[140px] text-[28px] font-extrabold   absolute"> 
+          <span className="text-color md:text-2xl efecto2 md:left-[185px] hidden md:flex  left-[140px] text-[28px] font-extrabold   absolute"> 
             Alexis
           </span>
-          <span className="text-color md:text-2xl efecto3 md:left-[310px] left-[235px]  text-[28px] font-extrabold  absolute">
+          <span className="text-color md:text-2xl efecto3 md:left-[310px] left-[200px]  text-[28px] font-extrabold  absolute">
             Lopez
           </span>
           <span className="text-color md:text-2xl efecto4 md:left-[420px] hidden md:flex right-[60px] font-extrabold text-[28px]  absolute">
             Santos
           </span>
         </h1>
-        <IconSlash stroke={3} size={40} className="absolute md:left-134 right-6 aparecer title-color" />
-        <IconChevronRight stroke={3} size={40} className="absolute md:left-140 right-0  aparecer title-color" />
+        <IconSlash stroke={3} size={40} className="absolute md:left-134 left-[280px] aparecer title-color" />
+        <IconChevronRight stroke={3} size={40} className="absolute md:left-140 left-[300px]  aparecer title-color" />
       </div>
       <div className="self-center p-4 pt-10 ">
         <h2 className="self-center md:text-3xl text-2xl font-black text-color">Desarrollardor web fullstack</h2>
