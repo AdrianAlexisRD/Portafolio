1f72d9af4e796c6c338d9891b53d6638b545e24b
Alex09121995 <158055482+Alex09121995@users.noreply.github.com>
2025-07-07 20:58:55 -0400

    se modifico home page

--- src/pages/AbouteMe.jsx
+++ src/pages/AbouteMe.jsx
@@ -16,23 +16,23 @@ export default function Home() {
       <div className="text-white flex justify-center mt-4 mb-4 md:mt-[10%] relative ">
         <IconChevronLeft stroke={3} size={40} className="absolute md:left-5 left-0 aparecer title-color" />
         <h1>
-          <span className="text-color md:text-3xl efecto1 md:left-[60px] left-[40px] text-[28px] font-extrabold absolute "> 
+          <span className="text-color md:text-2xl efecto1 md:left-[60px] left-[40px] text-[28px] font-extrabold absolute "> 
             Adrian
           </span>
-          <span className="text-color md:text-3xl efecto2 md:left-[185px] left-[140px] text-[28px] font-extrabold   absolute"> 
+          <span className="text-color md:text-2xl efecto2 md:left-[185px] left-[140px] text-[28px] font-extrabold   absolute"> 
             Alexis
           </span>
-          <span className="text-color md:text-3xl efecto3 md:left-[310px] left-[235px]  text-[28px] font-extrabold  absolute">
+          <span className="text-color md:text-2xl efecto3 md:left-[310px] left-[235px]  text-[28px] font-extrabold  absolute">
             Lopez
           </span>
-          <span className="text-color md:text-3xl efecto4 md:left-[420px] hidden md:flex right-[60px] font-extrabold text-[28px]  absolute">
+          <span className="text-color md:text-2xl efecto4 md:left-[420px] hidden md:flex right-[60px] font-extrabold text-[28px]  absolute">
             Santos
           </span>
         </h1>
         <IconSlash stroke={3} size={40} className="absolute md:left-134 right-6 aparecer title-color" />
         <IconChevronRight stroke={3} size={40} className="absolute md:left-140 right-0  aparecer title-color" />
       </div>
-      <div className="self-center p-4 pt-20 ">
+      <div className="self-center p-4 pt-10 ">
         <h2 className="self-center md:text-3xl text-2xl font-black text-color">Desarrollardor web fullstack</h2>
         <p className=" md:text-2xl text-white ">
         Hola soy un desarrollar junior fullstack autodidactacon deseos de demostrar
